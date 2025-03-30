ea7ae78ecf7098b76be17afa8139344b103a6c52
Nitinpatil <141624952+Nitinpatil7@users.noreply.github.com>
2025-03-30 16:12:47 +0530

    Update Login.jsx

--- mindstream-frontend/src/pages/Login.jsx
+++ mindstream-frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const submithandler = async () => {
     if (logindata.logemail !== "" && logindata.logemail !== "") {
       try {
-        const res = await fetch("http://localhost:5000/auth/login", {
+        const res = await fetch("https://blog-application-backend-new.onrender.com/auth/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
