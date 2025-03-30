1f5a4247446783a2a27b2b48a8a672eaadec51a3
Nitinpatil <141624952+Nitinpatil7@users.noreply.github.com>
2025-03-30 15:33:56 +0530

    Update db.js

--- mindstream-backend/config/db.js
+++ mindstream-backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectdb = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/mindstream", {});
+    const conn = await mongoose.connect(process.env.MONGO_URI, {});
     return conn;
   } catch (error) {
     console.log(`DB Error : ${error.message}`)
